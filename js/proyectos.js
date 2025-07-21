ad51870d6131d501ac85a77cfd616725a9643b17
jgramajo-2023053 <jgramajo-2023053@kinal.edu.gt>
2025-07-21 04:14:01 -0600

    Se agregan proyectos e imagenes

---
+++ js/proyectos.js
@@ -0,0 +1,51 @@
+const proyectos = {
+  aguaComun: {
+    titulo: "AguaComún",
+    imagen: "images/AguaComun/main.jpg",
+    descripcion: "Plataforma para gestión comunitaria del agua, usando React y Node.js. Con sistemas de registro, login, recaudaciones, reportes, actividades, entre otras funciones para organizarce como comunidad.",
+    capturas: [
+      "images/AguaComun/captura1.jpg",
+      "images/AguaComun/captura2.jpg"
+    ],
+    github: [
+      { nombre: "Frontend", url: "https://github.com/ndelacruz-2023048/AguaComun_Frontend.git" },
+      { nombre: "Backend", url: "https://github.com/ndelacruz-2023048/AguaComun_Backend.git" }
+    ]
+  },
+  calculadora: {
+    titulo: "Calculadora",
+    imagen: "images/Calculadora/main.jpg",
+    descripcion: "Calculadora basada en la calculadora de Windows, realizada en JavaFx y ejecutada en Netbeans",
+    capturas: [
+      "images/Calculadora/captura1.jpg",
+      "images/Calculadora/captura2.jpg"
+    ],
+    github: [
+      { nombre: "Repositorio", url: "https://github.com/jgramajo-2023053/Calculadora-2023053.git" }
+    ]
+  },
+  kinalStore: {
+    titulo: "Kinal Store",
+    imagen: "images/KinalStore/main.jpg",
+    descripcion: "Crud con vista del sistema de un supermercado desde el rol de un empleado, realizada en JavaFx y ejecutada en Netbeans",
+    capturas: [
+      "images/KinalStore/captura1.jpg",
+      "images/KinalStore/captura2.jpg"
+    ],
+    github: [
+      { nombre: "Repositorio", url: "https://github.com/jgramajo-2023053/KinalStore2024-2023053.git" }
+    ]
+  },
+  hotelera: {
+    titulo: "Sistema de Hotelera",
+    imagen: "images/Hotelera/main.jpeg",
+    descripcion: "Sistema hotelero en JavaFx con login, registro y administración de habitaciones, dividido en frontend y backend.",
+    capturas: [
+      "images/Hotelera/captura1.jpeg",
+      "images/Hotelera/captura2.jpeg"
+    ],
+    github: [
+      { nombre: "Repositorio", url: "https://github.com/ndelacruz-2023048/Sistema-gestion-hotelera.git" }
+    ]
+  }
+};
